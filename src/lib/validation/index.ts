0396e5dcd814918434407721feeac199b7f0d097
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-06-12 11:41:16 +0300

    Created postValidation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const signUpValidation = z.object({
+export const SignUpValidation = z.object({
 	name: z
 		.string()
 		.min(2, { message: "Should be at least 2 characters." })
@@ -15,9 +15,22 @@ export const signUpValidation = z.object({
 		.min(8, { message: "Should be at least 8 characters." }),
 });
 
-export const signInValidation = z.object({
+export const SignInValidation = z.object({
 	email: z.string().email({ message: "Should be a valid email address." }),
 	password: z
 		.string()
 		.min(8, { message: "Should be at least 8 characters." }),
+});
+
+export const PostValidation = z.object({
+	caption: z
+		.string()
+		.min(5, { message: "Should be at least 5 characters." })
+		.max(2200, { message: "Over the limit of 2200 characters." }),
+	file: z.custom<File[]>(),
+	location: z
+		.string()
+		.min(2, { message: "Should be at least 2 characters." })
+		.max(100, { message: "Over the limit of 100 characters." }),
+	tags: z.string(),
 });
